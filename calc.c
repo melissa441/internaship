0a88384d3d0c2f5fd2c05abee9387acc395213c6
melissa441 <melissakeza2020@gmail.com>
2024-08-22 13:01:26 +0200

    continous operation

--- calc.c
+++ calc.c
@@ -1,33 +1,35 @@
 #include<stdio.h>
 void addition(double x,double y){
     double z=x+y;
-    printf("%lf",z);
+    printf("%lf \n",z);
 }
 void multiplication(double x,double y){
     double z=x*y;
-    printf("%lf",z);
+    printf("%lf \n",z);
 }
 void subtraction(double x,double y){
     double z=x-y;
-    printf("%lf",z);
+    printf("%lf \n",z);
 }
 void division(double x,double y){
     double z=x/y;
-    printf("%lf",z);
+    printf("%lf \n",z);
 }
-void modulus(double x,double y){
-    double z=x%y;
-    printf("%lf",z);
+void modulus(int x,int y){
+    int z=x%y;
+    printf("%d \n",z);
 }
 int main(){
     double a,b,ans;
     char operator;
+    int choice;
+    do{
+    printf("choose the operation with arthmetic operators:");
+    scanf("%c",&operator);
     printf("enter the first number:");
     scanf("%lf",&a);
     printf("enter the second number:");
     scanf("%lf",&b);
-    printf("choose the operation with arthmetic operators");
-    scanf("%c",&operator);
     switch(operator){
         case '+':addition(a,b);
         break;
@@ -42,5 +44,8 @@ int main(){
         default:printf("Invalid operator.");
         break;
     }
+    printf("Do you want to continue? enter 1 if not enter 0:");
+    scanf(" %d", &choice);
+    }while(choice==1);
     return 0;
 }
