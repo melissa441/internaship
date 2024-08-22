a80f91f73bd7b470bf93c7cce813209d399259cd
melissa441 <melissakeza2020@gmail.com>
2024-08-22 11:12:23 +0200

    calculator

---
+++ calc.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void addition(double x,double y){
+    double z=x+y;
+    printf("%lf",z);
+}
+void multiplication(double x,double y){
+    double z=x*y;
+    printf("%lf",z);
+}
+void subtraction(double x,double y){
+    double z=x-y;
+    printf("%lf",z);
+}
+void division(double x,double y){
+    double z=x/y;
+    printf("%lf",z);
+}
+int main(){
+    double a,b,ans;
+    char operator;
+    printf("enter the first number:");
+    scanf("%lf",&a);
+    printf("enter the second number:");
+    scanf("%lf",&b);
+    printf("choose the operation with arthmetic operators");
+    scanf("%c",&operator);
+    switch(operator){
+        case '+':addition(a,b);
+        break;
+        case '*':multiplication(a,b);
+        break;
+        case '-':subtraction(a,b);
+        break;
+        case '/':division(a,b);
+        break;
+        default:printf("Invalid operator.");
+        break;
+    }
+    return 0;
+}
